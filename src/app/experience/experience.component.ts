9eeb9535e1b3aaf2f87d7207f73ba33a1df4a1e3
bayrem <bayremboussaidi187@gmail.com>
2025-08-11 00:55:31 +0100

    'certs'

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -8,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class ExperienceComponent implements OnInit {
 
   experiences = [
-   
-    {
+
+        {
       id: '01',
+      title: 'Software Develop (Intern)',
+      company: 'Global Enr - lac 2 , tunis , tunisia',
+      date: 'Sep 2024 - Jul 2025',
+      description: 'Full-featured car rental platform with role-based access (admin, agency, customer, follower), enabling car management, chat, email notifications, and PDF+QR generation for rentals. Keycloak-secured routes support user login, online payments, rental validation, and interaction tracking via emails and real-time notifications..',
+      tools: ['Angular' , 'Spring Boot', 'gitlab' , 'AWS' , 'terraform' , 'keycloak' , 'MySQL' , 'Docker' ]
+    },
+    {
+      id: '02',
       title: 'Software Develop (Intern)',
       company: 'Global Enr - lac 2',
       date: 'juin 2024 - Aout 2024',
@@ -18,13 +26,14 @@ export class ExperienceComponent implements OnInit {
       tools: ['Docker', 'Jenkins', 'react' , 'node']
     },
     {
-      id: '02',
+      id: '03',
       title: 'data analyst (Intern)',
       company: 'clenolia france - tunis',
       date: 'July 2022 - August 2022',
       description: 'Developed python web scraping scripts to extract data from log files , working on redhat machines .',
       tools: ['python' , 'redhat']
     }
+
   ];
   constructor() { }
 
