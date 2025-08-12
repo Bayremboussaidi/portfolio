d589f6242b3082e907121eccf092c15c5940cc7e
bayrem <bayremboussaidi187@gmail.com>
2025-08-12 13:11:31 +0100

    'proj'

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/*import { Component, OnInit } from '@angular/core';
 import { ProjectDetailComponent } from '../project-detail/project-detail.component';
 import { MatDialog } from '@angular/material/dialog';
 
@@ -12,7 +12,7 @@ export class PortfolioComponent implements OnInit {
   constructor(public dialog: MatDialog) { }
 
   projects = [
-    
+
     {
       id: 0,
       name: 'LocationTN',
@@ -28,7 +28,7 @@ export class PortfolioComponent implements OnInit {
       date: "De 1 févr. 2022 À 23 févr. 2022",
       description: "This application is intended for clients, administrators, influencers, and advertisers of various brands and shops, to allow them to interact with each other in an automatically organized manner."
     },
-    
+
   ];
 
   currentImageIndex: { [key: number]: number } = {};
@@ -50,7 +50,7 @@ export class PortfolioComponent implements OnInit {
 
 
   openDialog(project: any): void {
-    document.body.classList.add('modal-open'); 
+    document.body.classList.add('modal-open');
     this.dialog.open(ProjectDetailComponent, {
       data: { project }
     }).afterClosed().subscribe(() => {
@@ -58,4 +58,120 @@ export class PortfolioComponent implements OnInit {
     });
   }
 
+}
+
+*/
+
+
+
+
+
+
+
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ProjectDetailComponent } from '../project-detail/project-detail.component';
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.css']
+})
+export class PortfolioComponent implements OnInit {
+
+  constructor(public dialog: MatDialog) {}
+
+  projects = [
+    {
+      id: 0,
+      name: 'LocationTN',
+      images: [
+        'assets/img/work/loc/1.PNG', 'assets/img/work/loc/2.PNG', 'assets/img/work/loc/3.PNG',
+        'assets/img/work/loc/4.PNG', 'assets/img/work/loc/5.PNG', 'assets/img/work/loc/6.PNG',
+        'assets/img/work/loc/7.PNG', 'assets/img/work/loc/8.PNG', 'assets/img/work/loc/9.PNG',
+        'assets/img/work/loc/10.PNG'
+      ],
+      technologies: ["Spring Boot 2.7", "Thymeleaf", "MySQL 5.7"],
+      description: "This project involves developing a web application named 'LocationTN' aimed at people looking to rent a car without wasting time, providing all necessary information for customer satisfaction. Managers can also use it to facilitate interaction with customers.",
+      codeLink: "https://github.com/yourusername/locationTN"
+    },
+    {
+      id: 1,
+      name: 'E-Store',
+      images: [
+        'assets/img/work/ecomm/1.png', 'assets/img/work/ecomm/2.png', 'assets/img/work/ecomm/3.png',
+        'assets/img/work/ecomm/4.png', 'assets/img/work/ecomm/5.png', 'assets/img/work/ecomm/6.png',
+        'assets/img/work/ecomm/7.png', 'assets/img/work/ecomm/8.png'
+      ],
+      technologies: ["Spring Boot 2.7", "Angular 12", "MySQL 5.7"],
+      description: "An e-commerce platform for clients, administrators, influencers, and advertisers to interact in an organized manner.",
+      codeLink: "https://github.com/yourusername/estore"
+    },
+
+        {
+      id: 1,
+      name: 'E-Store',
+      images: [
+        'assets/img/work/ecomm/1.png', 'assets/img/work/ecomm/2.png', 'assets/img/work/ecomm/3.png',
+        'assets/img/work/ecomm/4.png', 'assets/img/work/ecomm/5.png', 'assets/img/work/ecomm/6.png',
+        'assets/img/work/ecomm/7.png', 'assets/img/work/ecomm/8.png'
+      ],
+      technologies: ["Spring Boot 2.7", "Angular 12", "MySQL 5.7"],
+      description: "An e-commerce platform for clients, administrators, influencers, and advertisers to interact in an organized manner.",
+      codeLink: "https://github.com/yourusername/estore"
+    },
+
+
+        {
+      id: 1,
+      name: 'E-Store',
+      images: [
+        'assets/img/work/ecomm/1.png', 'assets/img/work/ecomm/2.png', 'assets/img/work/ecomm/3.png',
+        'assets/img/work/ecomm/4.png', 'assets/img/work/ecomm/5.png', 'assets/img/work/ecomm/6.png',
+        'assets/img/work/ecomm/7.png', 'assets/img/work/ecomm/8.png'
+      ],
+      technologies: ["Spring Boot 2.7", "Angular 12", "MySQL 5.7"],
+      description: "An e-commerce platform for clients, administrators, influencers, and advertisers to interact in an organized manner.",
+      codeLink: "https://github.com/yourusername/estore"
+    },
+
+
+        {
+      id: 1,
+      name: 'E-Store',
+      images: [
+        'assets/img/work/ecomm/1.png', 'assets/img/work/ecomm/2.png', 'assets/img/work/ecomm/3.png',
+        'assets/img/work/ecomm/4.png', 'assets/img/work/ecomm/5.png', 'assets/img/work/ecomm/6.png',
+        'assets/img/work/ecomm/7.png', 'assets/img/work/ecomm/8.png'
+      ],
+      technologies: ["Spring Boot 2.7", "Angular 12", "MySQL 5.7"],
+      description: "An e-commerce platform for clients, administrators, influencers, and advertisers to interact in an organized manner.",
+      codeLink: "https://github.com/yourusername/estore"
+    }
+  ];
+
+  currentImageIndex: { [key: number]: number } = {};
+
+  ngOnInit(): void {
+    this.projects.forEach(project => {
+      this.currentImageIndex[project.id] = 0;
+      setInterval(() => {
+        this.currentImageIndex[project.id] =
+          (this.currentImageIndex[project.id] + 1) % project.images.length;
+      }, 5000);
+    });
+  }
+
+  isActiveImage(projectId: number, imageIndex: number): boolean {
+    return this.currentImageIndex[projectId] === imageIndex;
+  }
+
+  openDialog(project: any): void {
+    document.body.classList.add('modal-open');
+    this.dialog.open(ProjectDetailComponent, {
+      data: { project }
+    }).afterClosed().subscribe(() => {
+      document.body.classList.remove('modal-open');
+    });
+  }
 }
